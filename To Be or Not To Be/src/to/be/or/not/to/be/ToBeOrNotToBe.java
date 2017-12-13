405de4030c6d7ff29a17d5dd50d6a055faa38632
matgut33 <mattgutkin@gmail.com>
2017-12-13 10:04:53 -0500

    Initialize

---
+++ To Be or Not To Be/src/to/be/or/not/to/be/ToBeOrNotToBe.java
@@ -0,0 +1,17 @@
+
+package to.be.or.not.to.be;
+
+/**
+ * @author Matthew Gutkin
+ *
+ */
+public class ToBeOrNotToBe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
