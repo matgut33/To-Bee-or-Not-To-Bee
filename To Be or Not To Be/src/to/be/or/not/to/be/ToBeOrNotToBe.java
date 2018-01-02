d2ea4ca8e90225a8bfa2d920a5fe19a775a8c169
matgut33 <mattgutkin@gmail.com>
2018-01-02 09:53:33 -0500

    Prepared project with text files

--- To Be or Not To Be/src/to/be/or/not/to/be/ToBeOrNotToBe.java
+++ To Be or Not To Be/src/to/be/or/not/to/be/ToBeOrNotToBe.java
@@ -5,13 +5,27 @@ package to.be.or.not.to.be;
  * @author Matthew Gutkin
  *
  */
+
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.File;
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ToBeOrNotToBe {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd  HH;mm;ss");
+        Date date = new Date();
+        String runDate = sdf.format(date);
+        new File("Output/" + runDate).mkdirs();
+        
     }
 
 }
