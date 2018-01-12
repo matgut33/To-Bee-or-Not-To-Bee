f4246e362c43877435c444ea93495fe44d434c0f
Matthew Gutkin <mattgutkin@gmail.com>
2018-01-12 09:49:58 -0500

    Box class addition

--- To Bee or Not To Bee/src/to/be/or/not/to/be/Box.java
+++ To Bee or Not To Bee/src/to/be/or/not/to/be/Box.java
@@ -1,10 +1,34 @@
 
 package to.be.or.not.to.be;
 
+import javafx.geometry.Point3D;
+
 /**
  * @author Matthew Gutkin
  *
  */
 public class Box {
+    private Point3D loc;
+    boolean blocked;
+    
+    public Box()
+    {
+        Point3D x = new Point3D(0,0,0);
+        setLoc(x);
+    }
+    
+    public Box(Point3D x)
+    {
+       setLoc(x);
+    }
+    
+    public Point3D getLoc() {
+        return loc;
+    }
 
+    public void setLoc(Point3D loc) {
+        this.loc = loc;
+    }
+    
+    
 }
