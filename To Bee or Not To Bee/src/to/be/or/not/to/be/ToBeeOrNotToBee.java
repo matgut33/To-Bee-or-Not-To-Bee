8dc3d4edac2be68e299df9f39cdce86563649051
Matthew Gutkin <mattgutkin@gmail.com>
2018-01-12 09:39:23 -0500

    read

--- To Bee or Not To Bee/src/to/be/or/not/to/be/ToBeeOrNotToBee.java
+++ To Bee or Not To Bee/src/to/be/or/not/to/be/ToBeeOrNotToBee.java
@@ -15,18 +15,23 @@ import java.text.DecimalFormat;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.io.FileNotFoundException;
 
 public class ToBeeOrNotToBee {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        //MATCH NUMBER TO WHICH SETUP TO USE
+        int z = 1;
+        
         DateFormat sdf = new SimpleDateFormat("MM-dd-yyyy  HH;mm;ss");
         Date date = new Date();
         String runDate = sdf.format(date);
         new File("Output/" + runDate).mkdirs();
-    
+        File f = new File("beesetup" + z + ".txt");
+        Scanner r = new Scanner(f);
     }
 
 }
